6ab6ca9c32f5f65306d1ce72b0f05df0c82b299d
Adriana Ghita <a.adrianaghita@gmail.com>
2023-11-23 15:32:29 +0200

    eliminate aos animations from projects section

--- src/components/Projects/ProjectItem/ProjectItem.tsx
+++ src/components/Projects/ProjectItem/ProjectItem.tsx
@@ -1,22 +1,11 @@
 import { Project } from "../ProjectsList/ProjectsList";
 import { Card } from "react-bootstrap";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import styles from "./ProjectItem.module.scss";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ProjectItem = ({ title, description, image, url }: Project) => {
-  useEffect(() => {
-    AOS.init();
-  }, []);
-
   return (
-    <div
-      className={styles[`project-item`]}
-      data-aos="flip-right"
-      data-aos-duration="900"
-    >
+    <div className={styles[`project-item`]}>
       <Card className={styles.card}>
         <Card.Title className={styles.title}>{title}</Card.Title>
         <Link to={`${url}`} target="_blank">
