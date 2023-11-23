a888e9e9107caab8bca068061f1c48a109cf3657
Adriana Ghita <a.adrianaghita@gmail.com>
2023-11-23 14:40:17 +0200

    Implement the navbar for smaller screens

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,10 +3,61 @@ import { FaGithubSquare } from "react-icons/fa";
 
 import styles from "./NavBar.module.scss";
 import { NavLink } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
+      <Navbar expand="lg" className="bg-body-tertiary d-md-none d-lg-none">
+        <Container>
+          <div>
+            <Navbar.Brand href="https://www.linkedin.com/in/adriana-ghita/">
+              <FaLinkedin className={styles.icon} />
+            </Navbar.Brand>
+            <Navbar.Brand href="https://github.com/adrianaghita">
+              <FaGithubSquare className={styles.icon} />
+            </Navbar.Brand>
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? styles[`active-link`] : styles[`inactive-link`]
+                }
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="skills"
+                className={({ isActive }) =>
+                  isActive ? styles[`active-link`] : styles[`inactive-link`]
+                }
+              >
+                Skills
+              </NavLink>
+              <NavLink
+                to="projects"
+                className={({ isActive }) =>
+                  isActive ? styles[`active-link`] : styles[`inactive-link`]
+                }
+              >
+                Projects
+              </NavLink>
+              <NavLink
+                to="contact"
+                className={({ isActive }) =>
+                  isActive ? styles[`active-link`] : styles[`inactive-link`]
+                }
+              >
+                Contact
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
       <ul className={styles[`navbar-list`]}>
         <div className={styles.icons}>
           <li>
